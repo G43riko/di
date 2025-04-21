06b913ae02e2d76272a1fe68eb3c176236294863
Gabriel Csollei <gcsollei@hotmail.com>
2025-04-21 15:14:29 +0200

    feat: update di module exports and add publish config
    - Refactored exports in `src/index.ts` for better type and module structure.
    - Added `publish` configuration to `deno.json` to include/exclude specific files.
    - Updated `deno.jsonc` tasks with a new `doc` generation command.
    - Adjusted `.gitignore` to exclude `docs` directory.

--- workspaces/di/src/index.ts
+++ workspaces/di/src/index.ts
@@ -1,4 +1,9 @@
-export * from "./injectable.decorator.ts";
-export * from "./injector.ts";
-export * from "./create-injector.ts";
-export * from "./root-injector.ts";
+export { SimpleInjector } from "./simple-injector.ts";
+export { type CustomProvider, type ProviderToken, type ProviderType, type Type, type TypeResolution } from "./types.ts";
+export {Injectable, type InjectableParams} from "./injectable.decorator.ts";
+export {type CreateInjectorParams, createInjector} from "./create-injector.ts";
+export {type Injector} from "./injector.ts";
+export {RootInjector} from "./root-injector.ts";
+export {InjectionToken} from "./injection-token.ts";
+export {inject} from "./injections.ts";
+export {Scope} from "./scope.ts";
